39cc4a13068f936f99b628e2fa596e881b6cbe99
Calgary Michael <cseth.michael@gmail.com>
2019-05-30 21:16:55 -0500

    altered model schema

--- webapp/restaurant/models.py
+++ webapp/restaurant/models.py
@@ -6,25 +6,49 @@ class RestaurantType(models.Model):
     slug = models.SlugField(db_index=True)
     description = models.CharField(max_length=50)
 
+    def __repr__(self):
+        return "<RestaurantType {}>".format(self.slug)
 
-class RestaurantContact(models.Model):
-    boro = models.CharField(choices=choices.Boro.choices(), null=True, max_length=25)
-    building_number = models.PositiveSmallIntegerField(null=True)
-    street = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=7)
-    phone = models.CharField(max_length=10)
+    def __str__(self):
+        return self.description
 
 
 class Restaurant(models.Model):
     restaurant_type = models.ForeignKey(RestaurantType, on_delete=models.CASCADE)
-    contact = models.OneToOneField(RestaurantContact, on_delete=models.CASCADE)
-    code = models.CharField(max_length=8)
+    code = models.CharField(max_length=8, unique=True)
     name = models.CharField(max_length=200)
+
+    def __repr__(self):
+        return "<Restaurant {}>".format(self.code)
+
+    def __str__(self):
+        return "{0} ({1})".format(self.name, self.code)
+
+
+class RestaurantContact(models.Model):
+    restaurant = models.OneToOneField(Restaurant, on_delete=models.CASCADE)
+    boro = models.CharField(choices=choices.Boro.choices(), null=True, max_length=25)
+    building_number = models.PositiveSmallIntegerField(null=True)
+    street = models.CharField(max_length=100, null=True)
+    zip_code = models.CharField(max_length=7, null=True)
+    phone = models.CharField(max_length=10, null=True)
+
+    def __repr__(self):
+        return "<RestaurantContact {} ({})>".format(self.boro, self.building_number)
+
+    def __str__(self):
+        return u"{0} ({1})".format(self.boro, self.building_number)
 
 
 class Grade(models.Model):
     slug = models.SlugField(db_index=True)
     label = models.CharField(max_length=50)
+
+    def __repr__(self):
+        return "<Grade {}>".format(self.label)
+
+    def __str__(self):
+        return self.label
 
 
 class Inspection(models.Model):
@@ -35,8 +59,21 @@ class Inspection(models.Model):
     grade = models.ForeignKey(Grade, null=True, on_delete=models.CASCADE)
     grade_date = models.DateField(null=True)
 
+    def __repr__(self):
+        return "<Inspection {}>".format(self.id)
+
+    def __str__(self):
+        return "{0} - {1}".format(self.restaurant.name, self.inspection_date)
+
 
 class Violation(models.Model):
+    inspection = models.OneToOneField(Inspection, on_delete=models.CASCADE)
     code = models.CharField(max_length=5)
     critical_rating = models.PositiveSmallIntegerField(choices=choices.CriticalRating.choices())
     description = models.CharField(max_length=100)
+
+    def __repr__(self):
+        return "<Violation {}>".format(self.code)
+
+    def __str__(self):
+        return "{0} - {1}".format(self.code, self.critical_rating)
