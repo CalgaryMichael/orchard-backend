7d2e5a9322bb5d7b55cd4f5d8fabce1cf0df03c4
Calgary Michael <cseth.michael@gmail.com>
2019-05-30 21:17:26 -0500

    built out the admin site

--- webapp/restaurant/admin.py
+++ webapp/restaurant/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+from . import models
 
-# Register your models here.
+
+@admin.register(models.RestaurantType)
+class RestaurantTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "slug", "description")
+
+
+class RestaurantContactInline(admin.StackedInline):
+    model = models.RestaurantContact
+
+
+@admin.register(models.Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "style")
+    inlines = (RestaurantContactInline,)
+
+    def style(self, obj):
+        if obj.restaurant_type:
+            return obj.restaurant_type.description
+
+
+@admin.register(models.Grade)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ("id", "slug", "label")
+
+
+class ViolationInline(admin.StackedInline):
+    model = models.Violation
+
+
+@admin.register(models.Inspection)
+class InspectionAdmin(admin.ModelAdmin):
+    list_display = ("id", "restaurant_link", "inspection_type", "inspection_date", "grade")
+    list_filter = ("grade",)
+    inlines = (ViolationInline,)
+
+    def restaurant_link(self, obj):
+        url = reverse("admin:restaurant_restaurant_changelist") + "?id__exact={}".format(obj.restaurant.id)
+        return mark_safe("<a href='{0}'>{1}</a>".format(url, obj.restaurant))
+
+    restaurant_link.short_description = "Restaurant"
